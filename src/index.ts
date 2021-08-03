4237f9922d7f77510f7ff95ff65f499bb9dc73df
Donovan Hare <contact@donovanha.re>
2021-08-03 10:50:13 +0100

    Rewrite API, Add client config

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,59 @@
 import XAPI, { Activity, Actor, Context, Statement, Verb } from "@xapi/xapi";
+import { AxiosPromise } from "axios";
 import store from 'store2';
 
 export { generateAnonymousAgentObject, generateCourseContextObject, generateCourseObject } from './helpers';
 
-const endpoint = process.env.REACT_APP_CONKER_API_URL ?? '';
-const auth = process.env.REACT_APP_CONKER_API_KEY ?? '';
-const xapi: XAPI = new XAPI(endpoint, auth);
-
-export const CourseVerbs = XAPI.Verbs;
-
-export const reportActivity = (
-    learner: Actor,
-    action: Verb,
-    course: Activity,
-    context?: Context
-) => {
-    const xApiStatement: Statement = {
-        actor: learner,
-        verb: action,
-        object: course,
-        context: context
-    };
-
-    return xapi.sendStatement(xApiStatement);
+type IConker = {
+    _client: XAPI | null;
+    init: (opts: IConkerInit) => XAPI;
+    report: (learner: Actor, action: Verb, course: Activity, context?: Context) => AxiosPromise<string[]> | void;
+    clearLocalData: () => void;
 }
 
-export const clearActionData = () => {
-  store.clearAll();
-};
+type IConkerInit = {
+    endpoint: string, 
+    user: string, 
+    pass: string
+}
+
+export const Conker: IConker = {
+    _client: null,
+
+    init: (opts: IConkerInit) => {
+        if (Conker._client) {
+            console.warn('Conker.init() was called multiple times, ignoring...')
+            return Conker._client
+        }
+        let auth = XAPI.toBasicAuth(opts.user, opts.pass)
+        Conker._client = new XAPI(opts.endpoint, auth);
+        return Conker._client
+    },
+
+    report: (
+        learner: Actor,
+        action: Verb,
+        course: Activity,
+        context?: Context
+    ) => {
+        if (!Conker._client) {
+            console.error('No conker client initiated. Have you called Conker.init()?');
+            return;
+        }
+
+        const xApiStatement: Statement = {
+            actor: learner,
+            verb: action,
+            object: course,
+            context: context
+        };
+    
+        return Conker._client.sendStatement(xApiStatement);
+    },
+
+    clearLocalData: () => {
+        store.clearAll();
+    }
+}
+
+export const CourseVerbs = XAPI.Verbs;
